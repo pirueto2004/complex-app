d7df8c3370b1db71a1fcd145cdccfc0a76363932
pirueto2004 <juliobell2014@gmail.com>
2020-06-25 21:04:07 -0400

    Updated db.js

--- db.js
+++ db.js
@@ -14,19 +14,11 @@ let port = process.env.PORT
 const mongodbPassword = process.env.MONGOPASSWORD
 
 //Connection string
-// const connectionString = `mongodb+srv://todoAppUser:${mongodbPassword}@jbcluster-v5kqr.mongodb.net/${database_name}?retryWrites=true&w=majority`
 const MONGODB_URI = process.env.CONNECTIONSTRING
-// const MONGODB_URI = `mongodb+srv://todoAppUser:${mongodbPassword}@jbcluster-v5kqr.mongodb.net/${database_name}?retryWrites=true&w=majority`
-
-//if there's a shell environment variable named MONGODB_URI (deployed), use it; otherwise, connect to MongoDB Atlas database
-// let dbUrl = process.env.MONGODB_URI || connectionString
-// const dbUrl = connectionString
-
 
 //Connecting our app to mongoDB Atlas
 mongodb.connect(MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true}, (err, client) => {
-
-    
+  
     //Exports our MongoDB database object
     module.exports = client
 
@@ -34,6 +26,5 @@ mongodb.connect(MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
     const app = require('./app')
 
     //Once MongoDB establishes a connection, server starts listening on port PORT
-    // app.listen(port, () => console.log(`Server listening on PORT ${port}`))
     app.listen(port, () => console.log(`Server listening on PORT ${port}`))
 })
