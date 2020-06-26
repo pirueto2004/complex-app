c3b1d790ca9f31307e135b8ef05bc2f415316e59
pirueto2004 <juliobell2014@gmail.com>
2020-06-25 20:59:35 -0400

    Updated .env and db.js files

--- db.js
+++ db.js
@@ -15,8 +15,8 @@ const mongodbPassword = process.env.MONGOPASSWORD
 
 //Connection string
 // const connectionString = `mongodb+srv://todoAppUser:${mongodbPassword}@jbcluster-v5kqr.mongodb.net/${database_name}?retryWrites=true&w=majority`
-// const MONGODB_URI = process.env.CONNECTIONSTRING
-const MONGODB_URI = `mongodb+srv://todoAppUser:${mongodbPassword}@jbcluster-v5kqr.mongodb.net/${database_name}?retryWrites=true&w=majority`
+const MONGODB_URI = process.env.CONNECTIONSTRING
+// const MONGODB_URI = `mongodb+srv://todoAppUser:${mongodbPassword}@jbcluster-v5kqr.mongodb.net/${database_name}?retryWrites=true&w=majority`
 
 //if there's a shell environment variable named MONGODB_URI (deployed), use it; otherwise, connect to MongoDB Atlas database
 // let dbUrl = process.env.MONGODB_URI || connectionString
